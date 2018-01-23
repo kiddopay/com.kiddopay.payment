cc7e011b198222004f42b8c8f6af1d80dd769461
pinkgorilla <tris.setiawan@yahoo.com>
2018-01-24 00:53:32 +0700

    initial commit

---
+++ Com.KiddoPay.Payment.Service/Controllers/IdentityController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Com.KiddoPay.Payment.Service.Controllers
+{
+    [Route("identity")]
+    [Authorize]
+    public class IdentityController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return new JsonResult(from claim in User.Claims select new { claim.Type, claim.Value });
+        } 
+    }
+}
